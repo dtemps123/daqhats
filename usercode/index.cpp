0dec248b5f617e4887e2fa750c2ae37d678a8785
dylan <temples@u.northwestern.edu>
2022-03-11 16:39:44 -0600

    change the call to new Function constructor

--- usercode/index.cpp
+++ usercode/index.cpp
@@ -49,7 +49,7 @@ Napi::Object Init(Napi::Env env, Napi::Object exports) {
   
   exports.Set(
     Napi::String::New(env, "setup"),
-    Napi::Function::New(env, setup)
+    Napi::Function::New<setup>(env)
   );
   
   // exports.Set(
